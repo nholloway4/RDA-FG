2c8b46315d57de43d7f8f06404195e5b00b22c62
Nicholas Holloway <nholloway4@gmail.com>
2016-07-24 00:11:14 -0400

    Adding gitignore

--- fiveguys/Gruntfile.js
+++ fiveguys/Gruntfile.js
@@ -31,6 +31,39 @@ module.exports = function(grunt) {
               }
             }            
         },
+        autoprefixer: {
+            dist: {
+              files: [
+                {
+
+                  expand: true,
+                  cwd: 'css/sass',
+                  src: ["*.scss"]
+                }
+              ]
+            }
+        },
+            watch: {
+            sass: {
+              files: ['build/global.css'],
+              tasks: ['autoprefixer'],                
+              options: {
+                livereload: true
+              }
+            }            
+        },
+        pleeease: {
+            custom: {
+              options: {
+                autoprefixer: {'browsers': ['last 4 versions']},
+                minifier: false
+                
+              },
+              files: {
+                'css/global.css': 'css/global.css'
+              }
+            }
+          },        
         concat: {   
             concatjs: {
                 src: [
@@ -50,12 +83,15 @@ module.exports = function(grunt) {
     });
 
     // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-autoprefixer'); 
+    grunt.loadNpmTasks('grunt-pleeease');
+    grunt.loadNpmTasks('grunt-postcss');
     grunt.loadNpmTasks('grunt-contrib-concat'); 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-sass');
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['sass', 'concat', 'uglify']);
+    grunt.registerTask('default', ['sass', 'pleeease', 'concat', 'uglify']);
 
 };
